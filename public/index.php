bbe762b6938035a5c9253ca1e67c41652bc64f43
Costin Moise <necenzurat@gmail.com>
2022-02-10 16:22:16 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
-header('Location: https://tipro.ro/?utm_source='.$actual_link.'&utm_medium=redirect&utm_campaign='.$_SERVER['SERVER_NAME'], ,TRUE, 302);
+header('Location: https://tipro.ro/?utm_source='.$actual_link.'&utm_medium=redirect&utm_campaign='.$_SERVER['SERVER_NAME'] ,TRUE, 302);
 header('X-love: necenzurat');
 
 exit;
