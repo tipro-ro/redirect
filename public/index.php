52dd76905373322e78610f7dcc4e5582edfe569d
Costin Moise <necenzurat@gmail.com>
2022-03-26 09:23:54 +0200

    fucking redirects

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php 
 
-$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://".$_SERVER[HTTP_HOST].$_SERVER[REQUEST_URI];
+$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 
 header('Location: https://tipro.ro/?utm_source='.$actual_link.'&utm_medium=redirect&utm_campaign='.$_SERVER['SERVER_NAME'] ,TRUE, 302);
 header('X-love: necenzurat');
